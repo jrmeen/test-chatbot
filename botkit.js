30f4648d233f57e0283d4c3ec0b6d0a5594611a2
Jirapt <59030143@kmitl.ac.th>
2020-04-21 04:49:45 +0700

    initial commit for webhook

---
+++ botkit.js
@@ -0,0 +1,24 @@
+let { Botkit } = require('botkit');
+
+const controller = new Botkit(MY_CONFIGURATION);
+
+controller.hears('hello','direct_message', function(bot, message) {
+    bot.reply(message,'Hello yourself!');
+});
+
+
+const { Botkit } = require('botkit');
+
+const controller = new Botkit({
+    webhook_uri: '/api/messages',
+});
+
+controller.hears('.*','message', async(bot, message) => {
+
+    await bot.reply(message, 'I heard: ' + message.text);
+
+});
+
+controller.on('event', async(bot, message) => {
+    await bot.reply(message,'I received an event of type ' + message.type);
+});
